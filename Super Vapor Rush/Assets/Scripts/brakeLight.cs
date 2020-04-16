08c490b781a54b314090dd396da20e9047263f3e
Nafis Hoque <nhoque2@student.gsu.edu>
2020-04-15 22:59:52 -0400

    added new version

---
+++ Super Vapor Rush/Assets/Scripts/brakeLight.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class brakeLight : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.DownArrow)||Input.GetKey(KeyCode.S)){
+	GetComponent<Light>().range=1.0f;
+	}
+	else{
+	GetComponent<Light>().range=0.5f;
+	}
+    }
+}
